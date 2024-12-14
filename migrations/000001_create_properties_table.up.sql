d3b99be973cbcf17e34fc35be036dcc61b8a3605
dizzy <diogonnobrega@gmail.com>
2024-12-14 01:50:28 +0000

    Add local database setup with make commands. Read db dsn from .env

---
+++ migrations/000001_create_properties_table.up.sql
@@ -0,0 +1,8 @@
+CREATE TABLE IF NOT EXISTS properties (
+    id bigserial PRIMARY KEY,
+    title text NOT NULL,
+    description text NOT NULL,
+    location text NOT NULL,
+    created_at timestamp(0) with time zone NOT NULL DEFAULT NOW(),
+    created_by text NOT NULL
+);
