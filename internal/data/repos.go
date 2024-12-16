68820814608d8b282abcb89fb2cf3600ee1b9d36
dizzy <diogonnobrega@gmail.com>
2024-12-16 22:33:51 +0000

    feat: Add tokens repo and migration.

--- internal/data/repos.go
+++ internal/data/repos.go
@@ -13,11 +13,13 @@ var (
 type Repos struct {
 	Properties PropertyRepo
 	Users      UserRepo
+	Tokens     TokenRepo
 }
 
 func NewRepos(db *sql.DB) Repos {
 	return Repos{
 		Properties: PropertyRepo{DB: db},
 		Users:      UserRepo{DB: db},
+		Tokens:     TokenRepo{DB: db},
 	}
 }
